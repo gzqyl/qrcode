0259d12b3c5f33216734b462a7ec7e80aade37df
gzqyl <test@qq.com>
2018-05-09 16:42:57 +0800

    v1.0.0

--- phpqrcode/addLogo.php
+++ phpqrcode/addLogo.php
@@ -5,6 +5,8 @@
 
 		public static function logo($data, $outfile, $errorCorrectionLevel, $matrixPointSize,$margin=2,$save=false,$bg=0xFFFFFF,$fg=0x000000,$logo){
 
+				QRcode::png($data, $outfile, $errorCorrectionLevel, $matrixPointSize, 2);
+
 				$logo_resized = imagecreatetruecolor(50,50);
 
                 $logos = imagecreatefromjpeg($logo);
